c1437390bc4ad27d74ae487996619331a59e6127
jaydenthelwell <jaydenthelwell02@gmail.com>
2023-09-20 14:22:09 +0100

    made controller

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.querySelector("#train-svg").parentElement.addEventListener("click", (e) => {
-    console.log("hi");
-    document.getElementById("train-door").classList.toggle("d-none");
-  });
-});
