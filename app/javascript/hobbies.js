a15ef8a90cd70dc924693f860f447ee44c5d5e5b
jaydenthelwell <jaydenthelwell02@gmail.com>
2023-10-02 12:46:32 +0100

    Added d-none class to hobbies png instead of including d-none in the js file

--- app/javascript/hobbies.js
+++ app/javascript/hobbies.js
@@ -1,11 +1,9 @@
-// document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
   const interests = document.getElementById("cv");
   const hobbies = document.querySelector(".hobbies-popup-container");
-
-  hobbies.classList.add("d-none");
 
   interests.addEventListener("click", () => {
     hobbies.classList.remove("d-none");
     console.log("hey");
   });
-// });
+});
