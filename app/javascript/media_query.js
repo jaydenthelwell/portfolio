1f0aaa35acc8aa9205bc2218b2bcc8dc8a66a467
jaydenthelwell <jaydenthelwell02@gmail.com>
2023-10-05 18:16:36 +0100

    changing scale

--- app/javascript/media_query.js
+++ app/javascript/media_query.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const viewportMetaTag = document.querySelector('meta[name="viewport"]');
+// document.addEventListener("DOMContentLoaded", function () {
+//   const viewportMetaTag = document.querySelector('meta[name="viewport"]');
 
-  if (controllerName === 'home' && actionName === 'index') {
-    viewportMetaTag.setAttribute("content", "width=device-width, initial-scale=0.25");
-  } else {
-    viewportMetaTag.setAttribute("content", "width=device-width, initial-scale=1");
-  }
-});
+//   if (controllerName === 'home' && actionName === 'index') {
+//     viewportMetaTag.setAttribute("content", "width=device-width, initial-scale=0.25");
+//   } else {
+//     viewportMetaTag.setAttribute("content", "width=device-width, initial-scale=1");
+//   }
+// });
