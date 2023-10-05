994c18da55fd472dd9797293f1c1a06136b155f0
jaydenthelwell <jaydenthelwell02@gmail.com>
2023-10-05 17:19:48 +0100

    changing scale

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def viewport_meta_tag
+    if controller_name == 'home' && action_name == 'index'
+      content_tag(:meta, nil, name: 'viewport', content: 'width=device-width, initial-scale=0.25')
+    else
+      content_tag(:meta, nil, name: 'viewport', content: 'width=device-width, initial-scale=1')
+    end
+  end
 end
