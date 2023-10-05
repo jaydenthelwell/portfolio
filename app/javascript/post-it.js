5fe3f792c5846a60cf468d2c41e3d0549eea6e75
jaydenthelwell <jaydenthelwell02@gmail.com>
2023-10-05 12:27:04 +0100

    changed cloud sizes, changed the pop up messages

--- app/javascript/post-it.js
+++ app/javascript/post-it.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   // const post = document.getElementById("closed-postit");
   const postit = document.querySelector(".post-it-container");
-  const postit2 = document.querySelector(".post-it-beach ");
+  const postit2 = document.querySelector(".post-it-beach");
+  const options = document.querySelector(".options ");
+  const signpost = document.querySelector(".sign-post");
   const button = document.getElementById("close-popup");
 
   // postit.classList.add("d-none");
@@ -11,15 +13,16 @@ document.addEventListener("DOMContentLoaded", function () {
     postit.classList.add("d-none");
     postit2.classList.remove("d-none");
   });
-
-  postit2.addEventListener("click", () => {
+  options.addEventListener("click", () => {
     postit.classList.remove("d-none");
+    signpost.classList.remove("d-none");
+    options.classList.add("d-none");
     // post.classList.add("d-none");
   });
-
   postit2.addEventListener("click", () => {
     postit.classList.remove("d-none");
     // post.classList.add("d-none");
     postit2.classList.add("d-none");
+    options.classList.add("d-none");
   });
 });
