881ea80e3a99bfaa6177973af1953e865d8a8ef0
jaydenthelwell <jaydenthelwell02@gmail.com>
2023-10-05 20:05:13 +0100

    changing scale

--- app/javascript/media_query.js
+++ app/javascript/media_query.js
@@ -1,9 +1,15 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const viewportMetaTag = document.querySelector('meta[name="viewport"]');
+document.addEventListener("DOMContentLoaded", function () {
+  const viewportMetaTag = document.querySelector('meta[name="viewport"]');
+  const controllerName = viewportMetaTag.getAttribute("data-controller");
+  const actionName = viewportMetaTag.getAttribute("data-action");
+  const navbarFont = document.querySelector('.navbar-container');
 
-//   if (controllerName === 'home' && actionName === 'index') {
-//     viewportMetaTag.setAttribute("content", "width=device-width, initial-scale=0.25");
-//   } else {
-//     viewportMetaTag.setAttribute("content", "width=device-width, initial-scale=1");
-//   }
-// });
+  if (controllerName === 'home' && actionName === 'index') {
+    console.log("hey");
+    viewportMetaTag.setAttribute("content", "width=device-width, initial-scale=0.25");
+    // navbarFont.id = "navbar-container-2";
+    navbarFont.style.fontSize = "60px";
+  } else {
+    viewportMetaTag.setAttribute("content", "width=device-width, initial-scale=1");
+  }
+});
